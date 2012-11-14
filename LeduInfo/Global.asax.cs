5e5efc1c2b13d5c42263c4a8ec61f632f1d659b2
robin.lu <robin.lu@sonymobile.com>
2012-11-14 17:42:33 +0800

    update for website

--- LeduInfo/Global.asax.cs
+++ LeduInfo/Global.asax.cs
@@ -33,7 +33,7 @@ namespace LeduInfo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "LeduIndex", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
 
