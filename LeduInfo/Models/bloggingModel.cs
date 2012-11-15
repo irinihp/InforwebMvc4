90cde15047497bbdd00edc6c7333fb9a900e81e5
robin.lu <robin.lu@sonymobile.com>
2012-11-15 17:22:17 +0800

    add blog model

---
+++ LeduInfo/Models/bloggingModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeduInfo.Models
+{
+    public class bloggingModel
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class MyClass
+    {
+        
+    }
+
+    public class BlogContext : DbContext
+    {
+        public BlogContext()
+       
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<bloggingModel> Blogtbl { get; set; }
+    }
+}
