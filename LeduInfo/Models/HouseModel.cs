0a39146bf290e87896315788910395f0e8242494
robin1111 <luhaibin731@sina.com>
2012-11-21 23:27:15 +0800

    add one model

---
+++ LeduInfo/Models/HouseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeduInfo.Models
+{
+    public class HouseModel
+    {
+        class House
+        {
+            public int HouseID { get; set; }
+            public string Address { get; set; }
+            public string  { get; set; }
+        }
+
+        class HouseOwner
+        {
+            public int OwnerID { get; set; }
+            public string OwnerName { get; set; }
+        }
+
+    }
+}
