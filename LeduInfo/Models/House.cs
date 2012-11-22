ffe0ae52219abbee5cd1e53cf5a93facc0f17133
robin.lu <robin.lu@sonymobile.com>
2012-11-22 15:40:07 +0800

    add new model for creating database

---
+++ LeduInfo/Models/House.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeduInfo.Models
+{
+    public class House
+    {
+
+        public class HouseInfo
+        {
+            [Key]
+            public int houseID { get; set; }
+            public string Address { get; set; }
+            public int ownerID { get; set; }
+            public int renterID { get; set; }
+            public bool rented { get; set; }
+            public int houseStyleID { get; set; }
+        }
+        public class OwnerInfo
+        {
+            public int ownerID { get; set; }
+            public string ownerName { get; set; }
+            public long phoneName { get; set; }
+        }
+
+        public class RenterInfo
+        {
+            public int renterID { get; set; }
+            public string renterName { get; set; }
+            public long renterPhoneNumber { get; set; }
+        }
+
+        public class HouseStyle
+        {
+
+            public int houseStyleID { get; set; }
+            public string houseStyle { get; set; }
+        }
+    }
+}
---
+++ LeduInfo/Models/leduInfoDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LeduInfo.Models
+{
+    public class leduInfoDB:DbContext
+    {
+        public DbSet<House.HouseInfo> HouseInfotbl { get; set; }
+        public DbSet<House.HouseStyle> HouseStyletbl { get; set; }
+        public DbSet<House.OwnerInfo> OwnerInfotbl { get; set; }
+        public DbSet<House.RenterInfo> RenterInfotbl { get; set; }
+    }
+}
